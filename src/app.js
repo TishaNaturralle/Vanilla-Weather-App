dc986cf52b69571ad5736478f6e01974ef9f91ff
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2023-08-02 05:36:00 +0200

    Updated windElement

--- src/app.js
+++ src/app.js
@@ -28,11 +28,12 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("wind");
   let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidty;
-  wind.innerHTML = Math.round(response.data.wind.speed);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "70311dba46678b98a833492e1d519ca7";
