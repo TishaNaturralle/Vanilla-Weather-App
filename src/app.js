b9237e3ba8b5ad3c4fe42c8bc9ee0962a7a727b1
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2023-08-02 03:18:55 +0200

    Renamed Style.css folder

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
 let apiKey = "dt7522ba4c017dfaoc53ab6bcb9a6246";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?
-query=Worcestor&key=dt7522ba4c017dfaoc53ab6bcb9a6246=${apiKey}&units=metric;'
+query=Worcestor&key=dt7522ba4c017dfaoc53ab6bcb9a6246=${apiKey}&units=metric`;
+
+console.log(apiUrl);
